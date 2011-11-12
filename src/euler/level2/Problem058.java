693f337dc1731778bbf521ffc599a47a2250d16c
Marc de Jonge <marc.dejonge@tno.nl>
2011-11-13 00:05:44 +0100

    Optimized the slowest problem

--- src/euler/level2/Problem058.java
+++ src/euler/level2/Problem058.java
@@ -3,21 +3,22 @@ package euler.level2;
 import euler.Problem;
 import euler.sequence.Primes;
 
-public class Problem058 extends Problem<Long> {
+public class Problem058 extends Problem<Integer> {
 	@Override
-	public Long solve() {
-		long nrValues = 1, nrPrimes = 0;
-		long currValue = 1, currAdd = 2;
-		while (nrPrimes == 0 || nrValues / nrPrimes < 10) {
+	public Integer solve() {
+		int nrValues = 1, nrPrimes = 0;
+		int currValue = 1, currAdd = 2;
+		while (nrPrimes == 0 || nrValues < nrPrimes) {
 			for (int i = 0; i < 4; i++) {
 				currValue += currAdd;
 				nrValues++;
 				if (Primes.isPrime(currValue)) {
-					nrPrimes++;
+					nrPrimes += 10;
 				}
 			}
 			currAdd += 2;
 		}
+//		System.out.println(currValue);
 		return currAdd - 1;
 	}
 }
