b3a8cccb101ef3cd8581d00e08745acd381473ee
Marc de Jonge <marc.dejonge@tno.nl>
2011-11-13 00:07:54 +0100

    Changed to use the MultiCoreProblem

--- src/euler/level8/Problem357.java
+++ src/euler/level8/Problem357.java
@@ -1,36 +1,35 @@
 package euler.level8;
 
-import euler.Problem;
+import euler.MultiCoreProblem;
 import euler.sequence.Primes;
 
-public class Problem357 extends Problem<Long> {
+public class Problem357 extends MultiCoreProblem {
 
-	private static final long LIMIT = 100000000l;
+	public static final long LIMIT = 100000000l;
 
-	@Override
-	public Long solve() {
-		long sum = 0;
-		for (long prime : new Primes()) {
-			if (prime >= LIMIT) {
-				break;
-			}
-
-			int n = (int) (prime - 1);
-			int d = 2;
-			boolean stillValid = true;
-			while (stillValid && d * d <= n) {
-				int div = n / d;
-				if (div * d == n && !Primes.isPrime(d + div)) {
-					stillValid = false;
-				}
-				d++;
-			}
-			if (stillValid) {
-				sum += n;
-			}
-		}
-
-		return sum;
+	public Problem357() {
+		super(new Primes());
 	}
 
+	@Override
+	public boolean handleNumber(long prime) {
+		if (prime >= LIMIT) {
+			return false;
+		}
+
+		int n = (int) (prime - 1);
+		int d = 2;
+		boolean stillValid = true;
+		while (stillValid && d * d <= n) {
+			int div = n / d;
+			if (div * d == n && !Primes.isPrime(d + div)) {
+				stillValid = false;
+			}
+			d++;
+		}
+		if (stillValid) {
+			result.addAndGet(n);
+		}
+		return true;
+	}
 }
