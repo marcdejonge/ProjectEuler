440e8b352929e62d67d5ccbf5a16b7b38b929932
unknown <jongemd1@PC-11206.tsn.tno.nl>
2011-12-20 10:14:01 +0100

    Optimized the IntHashSet implementation

--- src/euler/collection/IntHashSet.java
+++ src/euler/collection/IntHashSet.java
@@ -3,93 +3,83 @@ package euler.collection;
 import java.util.Arrays;
 
 public class IntHashSet {
-    private static class IntHashSetStore {
-        private int filled;
-        private final int mask;
-        private final int[] buckets;
-
-        IntHashSetStore(int size) {
-            if (size > 30) {
-                size = 30;
-            }
-
-            int length = 1 << size;
-            buckets = new int[length];
-            mask = length - 1;
-            filled = 0;
-        }
-
-        boolean add(int nr) {
-            int ix = findIx(nr);
-            if (buckets[ix] == 0) {
-                buckets[ix] = nr;
-                filled++;
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        void addAll(IntHashSetStore store) {
-            for (int nr : store.buckets) {
-                add(nr);
-            }
-        }
-
-        void clear() {
-            Arrays.fill(buckets, 0);
-            filled = 0;
-        }
-
-        boolean contains(int nr) {
-            return buckets[findIx(nr)] == nr;
-        }
-
-        int findIx(int nr) {
-            int ix = hash(nr);
-            while (buckets[ix] != 0 && buckets[ix] != nr) {
-                ix = ix + 1 & mask;
-            }
-            return ix;
-        }
-
-        int hash(int val) {
-            return val * 0xdeadbeef & mask;
-        }
-
-        boolean isFull() {
-            return buckets.length == filled;
-        }
-    }
-
-    private int storeSize;
-
-    private IntHashSetStore store;
+    private int filled, storeSize;
+    private int mask;
+    private int[] buckets;
 
     public IntHashSet() {
         this(4);
     }
 
     public IntHashSet(int size) {
-        store = new IntHashSetStore(size);
+        if (size > 30) {
+            size = 30;
+        }
+
+        int length = 1 << size;
+        buckets = new int[length];
+        mask = length - 1;
+        filled = 0;
         storeSize = size;
     }
 
     public boolean add(int nr) {
-        if (store.isFull()) {
-            IntHashSetStore oldStore = store;
-            storeSize++;
-            store = new IntHashSetStore(storeSize);
-            store.addAll(oldStore);
+        checkFilled();
+
+        int ix = findIx(nr);
+        if (buckets[ix] == 0) {
+            buckets[ix] = nr;
+            filled++;
+            return true;
+        } else {
+            return false;
         }
-        return store.add(nr);
+    }
+
+    public void addAll(IntHashSet store) {
+        for (int nr : store.buckets) {
+            add(nr);
+        }
+    }
+
+    private final void checkFilled() {
+        if (isFull()) {
+            int[] oldNrs = buckets;
+            storeSize++;
+            int length = 1 << storeSize;
+            buckets = new int[length];
+            mask = length - 1;
+            filled = 0;
+            for (int n : oldNrs) {
+                if (n != 0) {
+                    add(n);
+                }
+            }
+        }
     }
 
     public void clear() {
-        store.clear();
+        Arrays.fill(buckets, 0);
+        filled = 0;
     }
 
     public boolean contains(int nr) {
-        return store.contains(nr);
+        return buckets[findIx(nr)] == nr;
+    }
+
+    private int findIx(int nr) {
+        int ix = hash(nr);
+        while (buckets[ix] != 0 && buckets[ix] != nr) {
+            ix = ix + 1 & mask;
+        }
+        return ix;
+    }
+
+    private int hash(int val) {
+        return val * 0xdeadbeef & mask;
+    }
+
+    private boolean isFull() {
+        return buckets.length == filled;
     }
 }
