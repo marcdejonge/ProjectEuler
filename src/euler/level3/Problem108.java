04cc27854b743557073592b794baaeedc8536133
Marc de Jonge <marc.dejonge@tno.nl>
2011-12-24 16:15:52 +0100

    Solved problem 108 and 110

---
+++ src/euler/level3/Problem108.java
@@ -0,0 +1,61 @@
+package euler.level3;
+
+import java.math.BigInteger;
+
+import euler.Problem;
+import euler.sequence.Primes;
+
+public class Problem108 extends Problem<Long> {
+    private final BigInteger[] primes;
+
+    private final int max;
+
+    public Problem108() {
+        this(1000);
+    }
+
+    public Problem108(int max) {
+        primes = new BigInteger[1000];
+        Primes ps = new Primes();
+        for (int ix = 0; ix < primes.length; ix++) {
+            primes[ix] = BigInteger.valueOf(ps.next());
+        }
+        this.max = max;
+    }
+
+    private BigInteger minimalNumber(long minDivsOfn2, BigInteger n, long divsOfn2, BigInteger currentMin, int maxTimes, int depth) {
+        if (n.compareTo(currentMin) > 0) {
+            return currentMin;
+        } else if (divsOfn2 >= minDivsOfn2) {
+            return n;
+        } else {
+            BigInteger min = currentMin;
+            BigInteger prime = primes[depth];
+            for (int times = 2; times <= maxTimes && n.compareTo(min) < 0; times += 2) {
+                n = n.multiply(prime);
+                BigInteger next = minimalNumber(minDivsOfn2,
+                                                n,
+                                                divsOfn2 * (times + 1),
+                                                currentMin.compareTo(min) < 0 ? currentMin : min,
+                                                times,
+                                                depth + 1);
+                if (next.compareTo(min) <= 0) {
+                    min = next;
+                } else {
+                    break;
+                }
+            }
+            return min;
+        }
+    }
+
+    private long nrOfSolutions(int minDivs) {
+        return minimalNumber(minDivs * 2, BigInteger.ONE, 1, BigInteger.valueOf(Long.MAX_VALUE), Integer.MAX_VALUE, 0).longValue();
+    }
+
+    @Override
+    public Long solve() {
+        return nrOfSolutions(max);
+    }
+
+}
---
+++ src/euler/level3/Problem110.java
@@ -0,0 +1,7 @@
+package euler.level3;
+
+public class Problem110 extends Problem108 {
+    public Problem110() {
+        super(4000000);
+    }
+}
