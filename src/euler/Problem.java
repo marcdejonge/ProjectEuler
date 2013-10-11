eb3a59317ca165a30102bbe63c055084dfc001b4
Marc de Jonge <marcdejonge@gmail.com>
2013-10-11 08:17:34 +0200

    Solved problem 106

--- src/euler/Problem.java
+++ src/euler/Problem.java
@@ -113,6 +113,7 @@ public abstract class Problem<T> {
         knownSolutions.put(103, Number.valueOf(20313839404245l));
         knownSolutions.put(104, Number.valueOf(329468));
         knownSolutions.put(105, Number.valueOf(73702));
+        knownSolutions.put(106, Number.valueOf(21384));
         knownSolutions.put(108, Number.valueOf(180180));
         knownSolutions.put(110, Number.valueOf(9350130049860600l));
         knownSolutions.put(111, Number.valueOf(612407567715l));
